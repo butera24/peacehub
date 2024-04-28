462de42ffad1496153638f678b154edb7e87a63e
KEjonathan <kejonathan75@gmail.com>
2024-04-28 15:20:38 +0300

    error fixed

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,8 +6,5 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    public funtion add(){
-        $data = Student::find(all);
-        return view('data')->with($data);
-    }
+
 }
